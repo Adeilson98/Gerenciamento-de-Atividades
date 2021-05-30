aab0b4e79ac6f6aea4f7127f66fe2e42c0028257
Adeilson Silva <adeilson.129@gmail.com>
2021-05-30 13:36:59 -0300

    Menu Finalizado e Funcionando

---
+++ MAPA.c
@@ -0,0 +1,14 @@
+menu(){
+	printf("******************************************\n");
+	printf("*                                        *\n");
+	printf("*                                        *\n");
+	printf("*       GERENCIAMENTO DE MATRICULAS      *\n");
+	printf("*                                        *\n");
+	printf("*                                        *\n");
+	printf("******************************************\n");
+}
+
+main(){
+	return menu();
+}
+
