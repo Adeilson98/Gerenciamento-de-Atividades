c63e6fdaef2367a4e64491fbf9818442e6b24f94
Adeilson Jaime da Silva <AJSILVA@PORTOAS.COM.BR>
2021-06-10 13:04:11 -0300

    Função de Cadastro de Atividades Adicionado

--- MAPA.c
+++ MAPA.c
@@ -1,14 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+#define STR 30
+#define FILA 5
+
+struct tmatriculas {
+	int RA[FILA];
+	char nome_arquivo[FILA];
+	char extensao_arquivo[FILA];
+	int matricula_responsavel[FILA]; //numero da matricula do professor responsável pela correção
+	int ini;
+	int fim;
+};
+
+struct tmatriculas fila;
+
 menu(){
 	printf("******************************************\n");
 	printf("*                                        *\n");
 	printf("*                                        *\n");
-	printf("*       GERENCIAMENTO DE MATRICULAS      *\n");
+	printf("*       GERENCIAMENTO DE ATIVIDADES      *\n");
 	printf("*                                        *\n");
 	printf("*                                        *\n");
 	printf("******************************************\n");
 }
+opcoes(){
+	printf("\n1 - Matricular\n");
+}
+
+void fila_matricular(){
+	if(fila.fim == FILA){
+		printf("\n Numero total de matriculas atingido!\n\n");
+		system("pause");
+	}
+	else{
+		printf("Digite o RA: ");
+		scanf("%d", &fila.RA[fila.fim]);
+		printf("Escreva o nome do Arquivo: ");
+		scanf("%s", &fila.nome_arquivo[fila.fim]);
+		printf("Escreva a extensão do arquivo: ");
+		scanf("%s", &fila.extensao_arquivo[fila.fim]);
+		printf("Digite a Matricula do professor reponsável pela correção: ");
+		scanf("%d", &fila.matricula_responsavel[fila.fim]);
+		fila.fim++;
+	}
+}
 
 main(){
-	return menu();
+	int opcao;
+	
+	do{
+		menu();
+		opcoes();
+		printf("Escolha uma opção: ");
+		scanf("%d", &opcao);
+		switch(opcao){
+			case 1:
+				fila_matricular();
+		}		break;
+	}while(opcao != 5);
 }
 
